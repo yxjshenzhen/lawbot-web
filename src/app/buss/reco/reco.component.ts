1ed2d4863ca28b05cacc1d0ee0f37f7661aeabda
ruiyun051@gmail.com <ruiyun051@gmail.com>
2018-07-26 11:26:36 +0800

    add chatbot icon

--- src/app/buss/reco/reco.component.ts
+++ src/app/buss/reco/reco.component.ts
@@ -145,8 +145,10 @@ export class RecoComponent implements OnInit {
       let data = res.data, code = res.code;
       if(code == 200){
         this.factors = data.caseKeys;  
-        this.loadLaws();
-        this.loadCaseRules();
+        if(this.factors.length > 0){
+          this.loadLaws();
+          this.loadCaseRules();
+        }
       }
       this.showFactors = true;
       this.caseKeysLoading = false;  
@@ -228,6 +230,11 @@ export class RecoComponent implements OnInit {
 
   copyRules(){
 
+  }
+
+  openLawbot(){
+    var v5_chat_attrs	= "toolbar=0,scrollbars=0,location=0,menubar=0,resizable=1,top=" + (window.screen.availHeight - (window.screen.availHeight/2+275+40)) + ",left=" + (window.screen.availWidth - (window.screen.availWidth/2+365+20)) + ",width=730,height=550";
+    window.open('/chatbot/desk/kehu.html?site_id=123898', '_blank', v5_chat_attrs);
   }
 
 }
