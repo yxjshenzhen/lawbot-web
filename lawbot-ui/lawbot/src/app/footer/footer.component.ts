b669157436e009a6f0c17c8ee60f771a7cdc129e
ruiyun051@gmail.com <ruiyun051@gmail.com>
2018-07-26 17:11:57 +0800

    reco

--- lawbot-ui/lawbot/src/app/footer/footer.component.ts
+++ lawbot-ui/lawbot/src/app/footer/footer.component.ts
@@ -13,4 +13,9 @@ export class FooterComponent implements OnInit {
   ngOnInit() {
   }
 
+  openLawbot(){
+    var v5_chat_attrs	= "toolbar=0,scrollbars=0,location=0,menubar=0,resizable=1,top=" + (window.screen.availHeight - (window.screen.availHeight/2+275+40)) + ",left=" + (window.screen.availWidth - (window.screen.availWidth/2+365+20)) + ",width=730,height=550";
+    window.open('/chatbot/desk/kehu.html?site_id=123898', '_blank', v5_chat_attrs);
+  }
+
 }
